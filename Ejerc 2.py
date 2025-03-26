2c318ec321719e8020ad7df1d33c0de9ee74de62
amanda9888 <anordesalas@gmail.com>
2025-03-26 15:54:01 -0400

    Create Ejerc 2.py

---
+++ Ejerc 2.py
@@ -0,0 +1,6 @@
+tp, tn, fp, fn = 40, 30, 20, 10
+
+accuracy = (tp + tn) / (tp + tn + fp + fn)  # 0.7
+precision = tp / (tp + fp)                   # 0.6667
+recall = tp / (tp + fn)                      # 0.8
+f1 = 2 * (precision * recall) / (precision + recall)  # 0.7273
